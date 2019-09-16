6064d0a63b8f187400939143b24fd6dddbebbb11
Andreea Zanfir <andreea.zanfir@softvision.ro>
2019-09-16 17:21:57 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by andreea.zanfir on 16/09/2019.
+//  Copyright © 2019 andreea.zanfir. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
